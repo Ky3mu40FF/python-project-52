2de670eb88360e5c0d2aa95f516e84c4d640421b
Aleksei Fominykh <foma1994@yandex.ru>
2023-03-05 18:18:12 +0300

    STEP-5 - Добавлены свойства verbose_name для полей модели Task

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,18 +1,36 @@
 """task_manager - tasks app - models module."""
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from task_manager.statuses.models import Status
 from task_manager.users.models import User
 
 class Task(models.Model):
     """Task model."""
 
-    name = models.CharField(max_length=100, null=False, blank=False)
-    description = models.TextField(max_length=1000, null=True, blank=True)
-    status = models.ForeignKey(Status, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(
+        max_length=100,
+        verbose_name=_('Password'),
+        null=False,
+        blank=False,
+    )
+    description = models.TextField(
+        max_length=1000,
+        verbose_name=_('Description'),
+        null=True,
+        blank=True,
+    )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        verbose_name=_('Status'),
+        null=False,
+        blank=False,
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.PROTECT,
         related_name='created_tasks',
+        verbose_name=_('Author'),
         null=False,
         blank=True,
     )
@@ -20,11 +38,12 @@ class Task(models.Model):
         User,
         on_delete=models.PROTECT,
         related_name='executing_tasks',
+        verbose_name=_('Executor'),
         null=True,
         blank=True,
     )
     # tags = models.ManyToManyField(Tag)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'),)
 
     def __str__(self):
         return self.name
