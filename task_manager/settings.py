8660cd849bcb2182141207413d1ff702d48eaeb4
Aleksei Fominykh <foma1994@yandex.ru>
2023-03-12 23:37:26 +0300

    STEP-7 - Добавлен пакет django-filter

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'django_filters',
     'task_manager',
     'task_manager.labels',
     'task_manager.statuses',
