80a7bda1b005d0b26ccfd8b29f37e458d2fb5d20
Aleksei Fominykh <foma1994@yandex.ru>
2023-03-20 23:39:30 +0300

    STEP-7 - Добавлены тесты на POST-запросы создания меток

--- tests/labels/test_labels_post.py
+++ tests/labels/test_labels_post.py
@@ -23,13 +23,82 @@ def test_create_valid_label(db, django_db_setup, client, label_model_test_fixtur
     )
 
     assert response.status_code == 302
-    # assert response.resolver_match.url_name == reverse_lazy('labels_create')
     assert response['Location'] == reverse_lazy('labels')
     assert Label.objects.count() == labels_count_before_creation+1
     assert Label.objects.last().name == label_data['name']
 
 
-def test_create_label_unauthorized(db, django_db_setup, client, label_model_test_fixtures) -> None:
+def test_create_missing_fields(db, django_db_setup, client, label_model_test_fixtures, user_model_test_fixtures) -> None:
+    label_data = label_model_test_fixtures['create']['missing_fields'].copy()
+    user_data = user_model_test_fixtures['login']['simple1'].copy()
+    client.login(
+        username=user_data['username'],
+        password=user_data['password'],
+    )
+    labels_count_before_creation = Label.objects.count()
+    response = client.post(
+        reverse_lazy('labels_create'),
+        data=label_data
+    )
+
+    errors = response.context['form'].errors
+    error_help = _('This field is required.')
+
+    assert 'name' in errors
+    assert [error_help] == errors['name']
+
+    assert response.status_code == 200
+    assert Label.objects.count() == labels_count_before_creation
+
+
+def test_create_existing_label(db, django_db_setup, client, label_model_test_fixtures, user_model_test_fixtures) -> None:
+    label_data = label_model_test_fixtures['create']['exists'].copy()
+    user_data = user_model_test_fixtures['login']['simple1'].copy()
+    client.login(
+        username=user_data['username'],
+        password=user_data['password'],
+    )
+    labels_count_before_creation = Label.objects.count()
+    response = client.post(
+        reverse_lazy('labels_create'),
+        data=label_data
+    )
+
+    errors = response.context['form'].errors
+    error_help = _('Label with this Name already exists.')
+
+    assert 'name' in errors
+    assert [error_help] == errors['name']
+
+    assert response.status_code == 200
+    assert Label.objects.count() == labels_count_before_creation
+
+
+def test_create_long_name(db, django_db_setup, client, label_model_test_fixtures, user_model_test_fixtures) -> None:
+    label_data = label_model_test_fixtures['create']['valid'].copy()
+    label_data['name'] = label_data['name'] * 100
+    user_data = user_model_test_fixtures['login']['simple1'].copy()
+    client.login(
+        username=user_data['username'],
+        password=user_data['password'],
+    )
+    labels_count_before_creation = Label.objects.count()
+    response = client.post(
+        reverse_lazy('labels_create'),
+        data=label_data
+    )
+
+    errors = response.context['form'].errors
+    error_help = _('Ensure this value has at most 100 characters ' f'(it has {len(label_data["name"])}).')
+
+    assert 'name' in errors
+    assert [error_help] == errors['name']
+
+    assert response.status_code == 200
+    assert Label.objects.count() == labels_count_before_creation
+
+
+def test_create_label_not_logged_in(db, django_db_setup, client, label_model_test_fixtures) -> None:
     label_data = label_model_test_fixtures['create']['valid'].copy()
     labels_count_before_creation = Label.objects.count()
     response = client.post(
@@ -41,3 +110,22 @@ def test_create_label_unauthorized(db, django_db_setup, client, label_model_test
     assert response['Location'] == reverse_lazy('login')
     assert Label.objects.count() != labels_count_before_creation+1
     assert Label.objects.last().name != label_data['name']
+
+
+def test_update_label_valid(db, django_db_setup, client, label_model_test_fixtures, user_model_test_fixtures) -> None:
+    label_data = label_model_test_fixtures['update']['valid'].copy()
+    user_data = user_model_test_fixtures['login']['simple1'].copy()
+    client.login(
+        username=user_data['username'],
+        password=user_data['password'],
+    )
+    labels_count_before_creation = Label.objects.count()
+    response = client.post(
+        reverse_lazy('labels_update', kwargs={'pk': 1}),
+        data=label_data
+    )
+
+    assert response.status_code == 302
+    assert response['Location'] == reverse_lazy('labels')
+    assert Label.objects.count() == labels_count_before_creation
+    assert Label.objects.get(pk=1).name == label_data['name']
