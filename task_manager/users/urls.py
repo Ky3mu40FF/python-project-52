caab5b8d4a7e8ba820152596595f678fd1aae448
Aleksei Fominykh <foma1994@yandex.ru>
2023-03-26 02:47:17 +0300

    Changed urls for login and logout

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('create/', views.UserCreationFormView.as_view(), name='users_create'),
     path('<int:pk>/update/', views.UserUpdateFormView.as_view(), name='users_update'),
     path('<int:pk>/delete/', views.UserDeleteFormView.as_view(), name='users_delete'),
-    path('login/', views.UserAuthenticationFormView.as_view(), name='login'),
-    path('logout/', views.UserLogOutView.as_view(), name='logout'),
 ]
